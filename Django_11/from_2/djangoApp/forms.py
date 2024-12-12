cb8ae6ba1fad3ec94f512b9b0e85a1ecc46e8102
Erfan <deverfan44@gmail.com>
2024-12-12 16:23:10 +0600

    Adding Django Form Api Different type of fromfield

--- Django_11/from_2/djangoApp/forms.py
+++ Django_11/from_2/djangoApp/forms.py
@@ -3,3 +3,13 @@ from django import forms
 class userForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
+    age = forms.IntegerField()
+    weight = forms.FloatField()
+    balance = forms.DecimalField()
+    check = forms.BooleanField(label='Are you female', required=False)
+    dob = forms.DateField()
+    appointment = forms.DateTimeField()
+    CHOICES = [('S','Smaill'), ('M','Medium'), ('L','Large')]
+    size = forms.ChoiceField(choices=CHOICES)
+    pizza = forms.MultipleChoiceField(choices=[('P','Pepproni'), ('M','Masrum'), ('B','Beaf')])
+    upload = forms.FileField(label='Upload your file', required=False)
--- Django_11/from_2/djangoApp/views.py
+++ Django_11/from_2/djangoApp/views.py
@@ -3,8 +3,12 @@ from . forms import userForm
 
 def djangoForm(request):
     if request.method == "POST":
-        form = userForm(request.POST)
+        form = userForm(request.POST, request.FILES)
         if form.is_valid():
+            file = form.cleaned_data['upload']
+            with open("./djangoApp/upload/"  + file.name, 'wb+') as destintaion:
+                for chunck in file.chunks():
+                    destintaion.write(chunck)
             print(form.cleaned_data) 
     else:
         form = userForm()
